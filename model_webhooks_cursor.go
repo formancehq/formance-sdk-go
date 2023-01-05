ede3aa1329aabfc61d247146f3adb174133b754c
flemzord <flemzord@users.noreply.github.com>
2023-01-05 09:41:47 +0000

    feat: branch feat/add_search_in_monorepo

--- model_webhooks_cursor.go
+++ model_webhooks_cursor.go
@@ -3,7 +3,7 @@ Formance Stack API
 
 Open, modular foundation for unique payments flows  # Introduction This API is documented in **OpenAPI format**.  # Authentication Formance Stack offers one forms of authentication:   - OAuth2 OAuth2 - an open protocol to allow secure authorization in a simple and standard method from web, mobile and desktop applications. <SecurityDefinitions /> 
 
-API version: v1.0.0-rc.1
+API version: develop
 Contact: support@formance.com
 */
 
@@ -20,15 +20,18 @@ var _ MappedNullable = &WebhooksCursor{}
 
 // WebhooksCursor struct for WebhooksCursor
 type WebhooksCursor struct {
-	HasMore *bool `json:"has_more,omitempty"`
+	HasMore bool `json:"hasMore"`
+	Data []WebhooksConfig `json:"data"`
 }
 
 // NewWebhooksCursor instantiates a new WebhooksCursor object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewWebhooksCursor() *WebhooksCursor {
+func NewWebhooksCursor(hasMore bool, data []WebhooksConfig) *WebhooksCursor {
 	this := WebhooksCursor{}
+	this.HasMore = hasMore
+	this.Data = data
 	return &this
 }
 
@@ -40,36 +43,52 @@ func NewWebhooksCursorWithDefaults() *WebhooksCursor {
 	return &this
 }
 
-// GetHasMore returns the HasMore field value if set, zero value otherwise.
+// GetHasMore returns the HasMore field value
 func (o *WebhooksCursor) GetHasMore() bool {
-	if o == nil || isNil(o.HasMore) {
+	if o == nil {
 		var ret bool
 		return ret
 	}
-	return *o.HasMore
+
+	return o.HasMore
 }
 
-// GetHasMoreOk returns a tuple with the HasMore field value if set, nil otherwise
+// GetHasMoreOk returns a tuple with the HasMore field value
 // and a boolean to check if the value has been set.
 func (o *WebhooksCursor) GetHasMoreOk() (*bool, bool) {
-	if o == nil || isNil(o.HasMore) {
+	if o == nil {
 		return nil, false
 	}
-	return o.HasMore, true
+	return &o.HasMore, true
 }
 
-// HasHasMore returns a boolean if a field has been set.
-func (o *WebhooksCursor) HasHasMore() bool {
-	if o != nil && !isNil(o.HasMore) {
-		return true
+// SetHasMore sets field value
+func (o *WebhooksCursor) SetHasMore(v bool) {
+	o.HasMore = v
+}
+
+// GetData returns the Data field value
+func (o *WebhooksCursor) GetData() []WebhooksConfig {
+	if o == nil {
+		var ret []WebhooksConfig
+		return ret
 	}
 
-	return false
+	return o.Data
 }
 
-// SetHasMore gets a reference to the given bool and assigns it to the HasMore field.
-func (o *WebhooksCursor) SetHasMore(v bool) {
-	o.HasMore = &v
+// GetDataOk returns a tuple with the Data field value
+// and a boolean to check if the value has been set.
+func (o *WebhooksCursor) GetDataOk() ([]WebhooksConfig, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return o.Data, true
+}
+
+// SetData sets field value
+func (o *WebhooksCursor) SetData(v []WebhooksConfig) {
+	o.Data = v
 }
 
 func (o WebhooksCursor) MarshalJSON() ([]byte, error) {
@@ -82,9 +101,8 @@ func (o WebhooksCursor) MarshalJSON() ([]byte, error) {
 
 func (o WebhooksCursor) ToMap() (map[string]interface{}, error) {
 	toSerialize := map[string]interface{}{}
-	if !isNil(o.HasMore) {
-		toSerialize["has_more"] = o.HasMore
-	}
+	toSerialize["hasMore"] = o.HasMore
+	toSerialize["data"] = o.Data
 	return toSerialize, nil
 }
 
