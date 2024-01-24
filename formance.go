800ef9cfab48463a311c62437bd24fae628fb283
NumaryBot <support@formance.com>
2024-01-24 07:21:12 +0000

    release(sdk): v2.0.0-beta.14

--- formance.go
+++ formance.go
@@ -163,10 +163,10 @@ func New(opts ...SDKOption) *Formance {
 	sdk := &Formance{
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
-			OpenAPIDocVersion: "v2.0.0-beta.13",
-			SDKVersion:        "v2.0.0-beta.13",
+			OpenAPIDocVersion: "v2.0.0-beta.14",
+			SDKVersion:        "v2.0.0-beta.14",
 			GenVersion:        "2.237.2",
-			UserAgent:         "speakeasy-sdk/go v2.0.0-beta.13 2.237.2 v2.0.0-beta.13 github.com/formancehq/formance-sdk-go/v2",
+			UserAgent:         "speakeasy-sdk/go v2.0.0-beta.14 2.237.2 v2.0.0-beta.14 github.com/formancehq/formance-sdk-go/v2",
 		},
 	}
 	for _, opt := range opts {
